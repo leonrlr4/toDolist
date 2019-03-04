7156bf360c3beade9380f2b08f09b49299f8fe47
leonrlr4 <leonrlr4@gmail.com>
2019-03-04 10:46:34 +0800

    db config

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/todo-api', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://leon:Oo850302@cluster0-z5rk4.mongodb.net/test?retryWrites=true', {useNewUrlParser: true});
 mongoose.Promise = Promise;
 
 
