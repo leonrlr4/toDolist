69ff3ecaef8dc26af54292560a33c7681b4e5b00
leonrlr4 <leonrlr4@gmail.com>
2019-03-04 11:15:46 +0800

    yes

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.set('debug', true);
+mongoose.set('debug', true );
 // mongoose.connect('mongodb://localhost/todo-api', {useNewUrlParser: true});
 mongoose.connect('mongodb+srv://leon:qazwsxedc@cluster0-z5rk4.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
 
